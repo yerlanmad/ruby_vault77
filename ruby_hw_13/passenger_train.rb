1e631232a279153ef2619d658bf34c633fdbce8e
yerlanmad <studyworld.m@gmail.com>
2020-05-07 22:12:20 +0600

    13 lesson

---
+++ ruby_hw_13/passenger_train.rb
@@ -0,0 +1,11 @@
+class PassengerTrain < Train
+  def initialize(train_number)
+    super
+    @train_type = "Passenger"
+  end
+
+  def attach_car(car)
+    return until car.is_a? PassengerCar
+    super
+  end
+end
---
+++ ruby_hw_13/station.rb
@@ -0,0 +1,39 @@
+class Station
+  attr_reader :station_name
+
+  def initialize(name)
+    @station_name = name
+    @trains = []
+  end
+
+  def to_s
+    @station_name
+  end
+  # Может принимать поезда (объекты класса Train, по одному за раз)
+
+  def accept_train(train)
+    @trains << train
+  end
+
+  def remove_train(train)
+    @trains.delete(train)
+  end
+
+  # Может возвращать список всех поездов на станции
+  # Может возвращать список поездов на станции по типу (см. ниже):
+  # должен вернуть поезда определенного типа, находящихся на станции.
+  def get_trains(type = "all")
+    if type == "all"
+      @trains
+    else
+      @trains.select { |el| el.train_type == type }
+    end
+  end
+
+  # Может отправлять поезда на другие станции маршрута (по одному за раз,
+  # при этом, поезд удаляется из списка поездов, находящихся на станции)
+  def send_train(train, station)
+    station.accept_train(train)
+    remove_train(train)
+  end
+end
